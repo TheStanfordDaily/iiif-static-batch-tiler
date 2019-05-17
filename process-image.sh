1d3bf770c153ddadadb6ecb14dbb82428241cc67
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-17 10:52:02 -0700

    fix setup and use azure cli

--- process-image.sh
+++ process-image.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 LINE=$((AWS_BATCH_JOB_ARRAY_INDEX + 1))
 DIR=$(sed -n ${LINE}p /tmp/files.txt)
-aws s3 ls s3://stanforddailyarchive/${DIR} | \
-    awk '{print $4}' | \
+aws s3 cp s3://stanforddailyarchive/${DIR} input/. --recursive
+    ls input |
     while read x ; do
     python iiif_static.py -d ${DIR} -p https://tiles.archives.stanforddaily.com -t 1024 -i $x $x ;
     done
-aws s3 cp out s3://stanforddailyarchive-tiled --recursive
+# aws s3 cp out s3://stanforddailyarchive-tiled --recursive
+az storage blob upload-batch -s out -d stanforddailyarchive-tiled
--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
 apt-get -y update
-apt-get install -y python3-pip python3-dev
+apt-get install -y python3-pip python3-dev cmake git libopenjp2-tools awscli curl
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
 cd /usr/local/bin
 ln -s /usr/bin/python3 python
 pip3 install --upgrade pip
 
-apt-get install -y awscli
-
-apt-get install -y cmake git libopenjp2-tools
 
 cd /tmp
 git clone https://github.com/zimeon/iiif.git
