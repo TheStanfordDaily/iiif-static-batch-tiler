79f85d5f37f16d6da5fd1d61f840d533f112c394
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-17 14:19:50 -0700

    fix: fix path

--- process-image.sh
+++ process-image.sh
@@ -10,7 +10,7 @@ aws s3 cp s3://stanforddailyarchive/${DIR}/Stanford_Daily-IMG/ input --recursive
 # Try one file
 # aws s3 cp s3://stanforddailyarchive/${DIR}/Stanford_Daily-IMG/Stanford_Daily_18921010_0001_0001.jp2 input/Stanford_Daily_18921010_0001_0001.jp2
 ls input | while read x ; do
-python iiif/iiif_static.py -d out/${DIR}/Stanford_Daily-IMG/ -p https://tiles.archives.stanforddaily.com/${DIR}/Stanford_Daily-IMG/ -t 1024 -i $x input/$x ;
+python iiif/iiif_static.py -d out/${DIR}/Stanford_Daily-IMG/ -p https://tiles.archives.stanforddaily.com/${DIR}/Stanford_Daily-IMG -t 1024 -i $x input/$x ;
 done
 
 # aws s3 cp out s3://stanforddailyarchive-tiled --recursive
