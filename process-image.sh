3a4eb5b3410f202174e4b4ff9aff466c2a2e3433
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-05-17 11:20:16 -0700

    feat: fix batch processing

--- process-image.sh
+++ process-image.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 LINE=$((AWS_BATCH_JOB_ARRAY_INDEX + 1))
 DIR=$(sed -n ${LINE}p /tmp/files.txt)
+cd /tmp
 aws s3 cp s3://stanforddailyarchive/${DIR} input/. --recursive
     ls input |
     while read x ; do
-    python iiif_static.py -d ${DIR} -p https://tiles.archives.stanforddaily.com -t 1024 -i $x $x ;
+    python iiif/iiif_static.py -d out/${DIR} -p https://tiles.archives.stanforddaily.com -t 1024 -i $x input/$x ;
     done
 # aws s3 cp out s3://stanforddailyarchive-tiled --recursive
 az storage blob upload-batch -s out -d stanforddailyarchive-tiled
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 AWS_BATCH_JOB_ARRAY_INDEX=0
-while [ $AWS_BATCH_JOB_ARRAY_INDEX -le 2 ]
+LINE_COUNT=$(wc -l files.txt | awk '{print $1'})
+while [ $AWS_BATCH_JOB_ARRAY_INDEX -le 0 ]
 do
-    docker run -i -e AWS_BATCH_JOB_ARRAY_INDEX=$AWS_BATCH_JOB_ARRAY_INDEX tsd-archives-tiler
+    docker run -i -e AWS_BATCH_JOB_ARRAY_INDEX=$AWS_BATCH_JOB_ARRAY_INDEX --env-file .env tsd-archives-tiler
     AWS_BATCH_JOB_ARRAY_INDEX=$((AWS_BATCH_JOB_ARRAY_INDEX + 1))
 done
